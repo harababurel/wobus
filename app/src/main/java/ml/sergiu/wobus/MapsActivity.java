cef7fc54baefd53f58af923caaedee0a60156c27
Sergiu Puscas <srg.pscs@gmail.com>
2018-01-03 22:59:58 +0200

    ouroieuroiewruoiewruoiruoiruoi

--- app/src/main/java/ml/sergiu/wobus/MapsActivity.java
+++ app/src/main/java/ml/sergiu/wobus/MapsActivity.java
@@ -1,124 +1,184 @@
 package ml.sergiu.wobus;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
-import android.widget.Toast;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-// https://stackoverflow.com/questions/33885378/animate-a-carmarker-along-a-path-in-google-map-android
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+import java.util.ArrayList;
+import java.util.List;
 
-    private static Location oldLocation;
-    private static float angle;
+// https://stackoverflow.com/questions/33885378/animate-a-carmarker-along-a-path-in-google-map-android
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    //    private static Location oldLocation;
+//    private static float angle;
     private GoogleMap mMap;
-    private LocationManager locationManager;
-    private Marker marker;
+//    private LocationManager locationManager;
+//    private Marker marker;
+
+
+    public void setAnimation(final List<LatLng> directionPoint) {
+        Marker marker = mMap.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_bus_marker))
+                .position(directionPoint.get(0))
+                .flat(true));
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(directionPoint.get(0), 10));
+        animateMarker(mMap, marker, directionPoint, false);
+    }
+
+
+    private static void animateMarker(GoogleMap myMap, final Marker marker, final List<LatLng> directionPoint,
+                                      final boolean hideMarker) {
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        Projection proj = myMap.getProjection();
+        final long duration = 30000;
+
+        final Interpolator interpolator = new LinearInterpolator();
+
+        handler.post(new Runnable() {
+            int i = 0;
+
+            @Override
+            public void run() {
+                long elapsed = SystemClock.uptimeMillis() - start;
+                float t = interpolator.getInterpolation((float) elapsed
+                        / duration);
+                if (i < directionPoint.size())
+                    marker.setPosition(directionPoint.get(i));
+                i++;
+
+
+                if (t < 1.0) {
+                    // Post again 16ms later.
+                    handler.postDelayed(this, 16);
+                } else {
+                    if (hideMarker) {
+                        marker.setVisible(false);
+                    } else {
+                        marker.setVisible(true);
+                    }
+                }
+            }
+        });
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_maps);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Log.w("BOBS","Fine location permission not enabled.");
-            Log.w("BOBS","Asking user for permission.");
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            return;
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2, 0, this);
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+//        LatLng sydney = new LatLng(-33.852, 151.211);
+//        googleMap.addMarker(new MarkerOptions().position(sydney)
+//                .title("Marker in Sydney"));
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
         mMap = googleMap;
+        List<LatLng> points = new ArrayList<>();
+        points.add(new LatLng(46.7712, 23.6236));
+        points.add(new LatLng(47.1855, 23.0573));
+        setAnimation(points);
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        locationUpdate(location);
-        if (oldLocation != null) {
-            double bearing = angleFromCoordinate(oldLocation.getLatitude(), oldLocation.getLongitude(), location.getLatitude(), location.getLongitude());
-            changeMarkerPosition(bearing);
-        }
-        oldLocation = location;
-    }
-
-    private void locationUpdate(Location location) {
-        LatLng latLng = new LatLng((location.getLatitude()), (location.getLongitude()));
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(latLng);
-        markerOptions.title("Current Position");
-        markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_bus_marker));
-        mMap.clear();
-        marker = mMap.addMarker(markerOptions);
-        CameraPosition position = CameraPosition.builder()
-                .target(new LatLng(location.getLatitude(), location.getLongitude()))
-                .zoom(19)
-                .tilt(30)
-                .build();
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(position));
-    }
-
-    private void changeMarkerPosition(double position) {
-        float direction = (float) position;
-        Log.e("LocationBearing", "" + direction);
-
-        if (direction == 360.0) {
-            //default
-            marker.setRotation(angle);
-        } else {
-            marker.setRotation(direction);
-            angle = direction;
-        }
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
-
-    private double angleFromCoordinate(double lat1, double long1, double lat2,
-                                       double long2) {
-        double dLon = (long2 - long1);
-
-        double y = Math.sin(dLon) * Math.cos(lat2);
-        double x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1)
-                * Math.cos(lat2) * Math.cos(dLon);
-
-        double brng = Math.atan2(x, y);
-
-        brng = Math.toDegrees(brng);
-        brng = (brng + 360) % 360;
-        brng = 360 - brng;
-        return brng;
-    }
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//    }
+//
+//    @Override
+//    public void onLocationChanged(Location location) {
+//        locationUpdate(location);
+//        if (oldLocation != null) {
+//            double bearing = angleFromCoordinate(oldLocation.getLatitude(), oldLocation.getLongitude(), location.getLatitude(), location.getLongitude());
+//            changeMarkerPosition(bearing);
+//        }
+//        oldLocation = location;
+//    }
+//
+//    private void locationUpdate(Location location) {
+//        LatLng latLng = new LatLng((location.getLatitude()), (location.getLongitude()));
+//        MarkerOptions markerOptions = new MarkerOptions();
+//        markerOptions.position(latLng);
+//        markerOptions.title("Current Position");
+//        markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_bus_marker));
+//        mMap.clear();
+//        marker = mMap.addMarker(markerOptions);
+//        CameraPosition position = CameraPosition.builder()
+//                .target(new LatLng(location.getLatitude(), location.getLongitude()))
+//                .zoom(19)
+//                .tilt(30)
+//                .build();
+//        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(position));
+//    }
+//
+//    private void changeMarkerPosition(double position) {
+//        float direction = (float) position;
+//        Log.e("LocationBearing", "" + direction);
+//
+//        if (direction == 360.0) {
+//            //default
+//            marker.setRotation(angle);
+//        } else {
+//            marker.setRotation(direction);
+//            angle = direction;
+//        }
+//    }
+//
+//    @Override
+//    public void onStatusChanged(String provider, int status, Bundle extras) {
+//
+//    }
+//
+//    @Override
+//    public void onProviderEnabled(String provider) {
+//
+//    }
+//
+//    @Override
+//    public void onProviderDisabled(String provider) {
+//
+//    }
+//
+//
+//    private double angleFromCoordinate(double lat1, double long1, double lat2,
+//                                       double long2) {
+//        double dLon = (long2 - long1);
+//
+//        double y = Math.sin(dLon) * Math.cos(lat2);
+//        double x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1)
+//                * Math.cos(lat2) * Math.cos(dLon);
+//
+//        double brng = Math.atan2(x, y);
+//
+//        brng = Math.toDegrees(brng);
+//        brng = (brng + 360) % 360;
+//        brng = 360 - brng;
+//        return brng;
+//    }
 }
